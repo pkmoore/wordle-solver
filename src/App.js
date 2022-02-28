548c23f623d369e762117bf920642a8ccc89e75f
Preston Moore <pkm266@nyu.edu>
2022-02-27 22:55:48 -0500

    First draft

--- src/App.js
+++ src/App.js
@@ -1,17 +1,167 @@
-import React from 'react';
-import './App.css';
-import LetterRow from './components/letterrow';
-import Possibilities from './components/possibilities';
+import React, { Component } from "react";
+import "./App.css";
+import LetterRow from "./components/letterrow";
+import Possibilities from "./components/possibilities";
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    letterRows: [
+      {
+        id: 0,
+        letters: [
+          { id: 0, value: "", adjective: "NotInWord" },
+          { id: 1, value: "", adjective: "NotInWord" },
+          { id: 2, value: "", adjective: "NotInWord" },
+          { id: 3, value: "", adjective: "NotInWord" },
+          { id: 4, value: "", adjective: "NotInWord" },
+        ],
+      },
+      {
+        id: 1,
+        letters: [
+          { id: 0, value: "", adjective: "NotInWord" },
+          { id: 1, value: "", adjective: "NotInWord" },
+          { id: 2, value: "", adjective: "NotInWord" },
+          { id: 3, value: "", adjective: "NotInWord" },
+          { id: 4, value: "", adjective: "NotInWord" },
+        ],
+      },
+      {
+        id: 2,
+        letters: [
+          { id: 0, value: "", adjective: "NotInWord" },
+          { id: 1, value: "", adjective: "NotInWord" },
+          { id: 2, value: "", adjective: "NotInWord" },
+          { id: 3, value: "", adjective: "NotInWord" },
+          { id: 4, value: "", adjective: "NotInWord" },
+        ],
+      },
+      {
+        id: 3,
+        letters: [
+          { id: 0, value: "", adjective: "NotInWord" },
+          { id: 1, value: "", adjective: "NotInWord" },
+          { id: 2, value: "", adjective: "NotInWord" },
+          { id: 3, value: "", adjective: "NotInWord" },
+          { id: 4, value: "", adjective: "NotInWord" },
+        ],
+      },
+      {
+        id: 4,
+        letters: [
+          { id: 0, value: "", adjective: "NotInWord" },
+          { id: 1, value: "", adjective: "NotInWord" },
+          { id: 2, value: "", adjective: "NotInWord" },
+          { id: 3, value: "", adjective: "NotInWord" },
+          { id: 4, value: "", adjective: "NotInWord" },
+        ],
+      },
+    ],
+  };
+
+  getInCorrectPlaceLetters() {
+    let letters = new Set();
+    for(let i = 0; i < this.state.letterRows.length; i++) {
+      this.state.letterRows[i].letters
+     .filter(l => l.value !== "" && l.adjective === "InCorrectPlace")
+     .forEach(l => letters.add(l));
+    }
+      console.log("InCorrectPlace", letters);
+      return [...letters];
+  }
+
+  getInWordLetters() {
+    let letters = new Set();
+    for(let i = 0; i < this.state.letterRows.length; i++) {
+      this.state.letterRows[i].letters
+     .filter(l => l.value !== "" && l.adjective === "InWord")
+     .forEach(l => letters.add(l));
+    }
+      console.log("Inword", letters);
+      return [...letters];
+  }
+
+  getNotInWordLetters() {
+    let letters = new Set();
+    for(let i = 0; i < this.state.letterRows.length; i++) {
+      this.state.letterRows[i].letters
+     .filter(l => l.value !== "" && l.adjective === "NotInWord")
+     .forEach(l => letters.add(l));
+    }
+    console.log("NotInWord", letters);
+    return [...letters];
+  }
+
+  handleChange = (letterRow, letter, e) => {
+    const data = e.nativeEvent.data;
+    if(!data) return;
+    const letterRows = [...this.state.letterRows];
+    const letterRowIndex = letterRows.indexOf(letterRow);
+    letterRows[letterRowIndex] = { ...letterRow };
+
+    const letters = [...letterRows[letterRowIndex].letters];
+    const letterIndex = letters.indexOf(letter);
+    letters[letterIndex] = { ...letter };
+    letterRows[letterRowIndex].letters = letters;
+
+    letters[letterIndex].value = e.nativeEvent.data.toUpperCase();
+    this.setState({ letterRows });
+  };
+
+  cycleAdjective(letterRow, letter) {
+    if(!letter.value) return;
+    const letterRows = [...this.state.letterRows];
+    const letterRowIndex = letterRows.indexOf(letterRow);
+    letterRows[letterRowIndex] = { ...letterRow };
+
+    const letters = [...letterRows[letterRowIndex].letters];
+    const letterIndex = letters.indexOf(letter);
+    letters[letterIndex] = { ...letter };
+    letterRows[letterRowIndex].letters = letters;
+
+    letters[letterIndex] = { ...letter };
+    if (letters[letterIndex].adjective === "NotInWord") {
+      letters[letterIndex].adjective = "InWord";
+    } else if (letters[letterIndex].adjective === "InWord") {
+      letters[letterIndex].adjective = "InCorrectPlace";
+    } else {
+      ///must be "InCorrectPlace"
+      letters[letterIndex].adjective = "NotInWord";
+    }
+    this.setState({ letterRows });
+  }
+
+  handleClick = (letterRow, letter, e) => {
+    // e.detail === 2 means a double click
+    if (e.detail === 2) {
+      this.cycleAdjective(letterRow, letter);
+    }
+  };
+
+  render() {
+    return (
       <React.Fragment>
-      <h1>Top</h1>
-      <div><LetterRow /></div>
-      <div><Possibilities /></div>
-      <h1>Bottom</h1>
+        <h1>Top</h1>
+        <div>
+          {this.state.letterRows.map((lr) => (
+            <LetterRow
+              id={lr.id}
+              key={lr.id}
+              letterRow={lr}
+              onChange={this.handleChange}
+              onClick={this.handleClick}
+            />
+          ))}
+        </div>
+        <div>
+          <Possibilities inCorrectPlaceLetters={this.getInCorrectPlaceLetters()}
+                         inWordLetters={this.getInWordLetters()}
+                         notInWordLetters={this.getNotInWordLetters()} />
+        </div>
+        <h1>Bottom</h1>
       </React.Fragment>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/possibilities.jsx
+++ src/components/possibilities.jsx
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import { getWords } from '../data/dictionary';
 
 class Possibilities extends Component {
-    state = {};
-
     render() {
-        return (
-            <h1>hello</h1>
+        console.log(this.getCandidateWords());
+        return (<ul>
+            {this.getCandidateWords().map((w, i) => <li key={i}>{w}</li>)}
+            </ul>
         );
+    };
+
+    getCandidateWords() {
+        const { inCorrectPlaceLetters, inWordLetters, notInWordLetters } = this.props;
+
+        const words = getWords();
+
+        return words.filter(w => {
+            for(let i = 0; i < inCorrectPlaceLetters.length; i++) {
+                let index = inCorrectPlaceLetters[i].id;
+                if(w[index] !== inCorrectPlaceLetters[i].value) {
+                    return false;
+                }
+            }
+
+            for(let i = 0; i < notInWordLetters.length; i++) {
+                if(w.includes(notInWordLetters[i].value)) {
+                    return false;
+                }
+            }
+
+            for(let i = 0; i < inWordLetters.length; i++) {
+                if(!w.includes(inWordLetters[i].value)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
     }
 }
+
 
 export default Possibilities;
