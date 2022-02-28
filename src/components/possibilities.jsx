4bd0eb15363395092dcca46d288fe38b9f883fe6
Preston Moore <pkm266@nyu.edu>
2022-02-27 23:46:31 -0500

    Don't show a massive list of candidates

--- src/components/possibilities.jsx
+++ src/components/possibilities.jsx
@@ -1,44 +1,51 @@
-import React, {Component} from 'react';
-import { getWords } from '../data/dictionary';
+import React, { Component } from "react";
+import { getWords } from "../data/dictionary";
 
 class Possibilities extends Component {
-    render() {
-        console.log(this.getCandidateWords());
-        return (<ul>
-            {this.getCandidateWords().map((w, i) => <li key={i}>{w}</li>)}
-            </ul>
-        );
-    };
-
-    getCandidateWords() {
-        const { inCorrectPlaceLetters, inWordLetters, notInWordLetters } = this.props;
-
-        const words = getWords();
-
-        return words.filter(w => {
-            for(let i = 0; i < inCorrectPlaceLetters.length; i++) {
-                let index = inCorrectPlaceLetters[i].id;
-                if(w[index] !== inCorrectPlaceLetters[i].value) {
-                    return false;
-                }
-            }
-
-            for(let i = 0; i < notInWordLetters.length; i++) {
-                if(w.includes(notInWordLetters[i].value)) {
-                    return false;
-                }
-            }
-
-            for(let i = 0; i < inWordLetters.length; i++) {
-                if(!w.includes(inWordLetters[i].value)) {
-                    return false;
-                }
-            }
-
-            return true;
-        });
+  render() {
+    let candidates = this.getCandidateWords();
+    if (candidates.length > 100) {
+      return <p> Too many possibilities to list... </p>;
+    } else {
+      return (
+        <ul>
+          {candidates.map((w, i) => (
+            <li key={i}>{w}</li>
+          ))}
+        </ul>
+      );
     }
-}
+  }
 
+  getCandidateWords() {
+    const { inCorrectPlaceLetters, inWordLetters, notInWordLetters } =
+      this.props;
+
+    const words = getWords();
+
+    return words.filter((w) => {
+      for (let i = 0; i < inCorrectPlaceLetters.length; i++) {
+        let index = inCorrectPlaceLetters[i].id;
+        if (w[index] !== inCorrectPlaceLetters[i].value) {
+          return false;
+        }
+      }
+
+      for (let i = 0; i < notInWordLetters.length; i++) {
+        if (w.includes(notInWordLetters[i].value)) {
+          return false;
+        }
+      }
+
+      for (let i = 0; i < inWordLetters.length; i++) {
+        if (!w.includes(inWordLetters[i].value)) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+  }
+}
 
 export default Possibilities;
